89c56baafd8d24e70d3ef35fb779a72d772c026a
eedris <alabialade@gmail.com>
2023-12-31 12:02:28 +0100

    Deploy website - based on bf06176e37c03a28db9e8501bf082cb395ea782d

---
+++ assets/js/5e95c892.53793f7e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkafterwind_doc=self.webpackChunkafterwind_doc||[]).push([[661],{1892:(e,r,s)=>{s.r(r),s.d(r,{default:()=>f});s(7294);var a=s(512),c=s(1944),d=s(5281),n=s(8790),t=s(6040),u=s(5893);function f(e){return(0,u.jsx)(c.FG,{className:(0,a.Z)(d.k.wrapper.docsPages),children:(0,u.jsx)(t.Z,{children:(0,n.H)(e.route.routes)})})}}}]);
