a4dcf8de4b22f47b8f529380432474761074a5b9
eedris <alabialade@gmail.com>
2023-12-31 06:45:52 +0100

    update .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\LENOVO\\Desktop\\dev\\os\\afterwind\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\LENOVO\\Desktop\\dev\\os\\afterwind\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\LENOVO\\Desktop\\dev\\os\\afterwind\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\LENOVO\\Desktop\\dev\\os\\afterwind\\src\\css\\custom.css"),
-];
